9081de963f219e3609b86c285a1def6e5b5a3f70
Siwa12100 <jean.marcillac12@gmail.com>
2023-11-29 12:38:47 +0100

    creation du plugin Lobor

---
+++ Lobor/src/main/java/dev/siwa/lobor/Lobor.java
@@ -0,0 +1,18 @@
+package dev.siwa.lobor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lobor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("(Temporaire Lobor) : Activation du plugin lobor. \n");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
