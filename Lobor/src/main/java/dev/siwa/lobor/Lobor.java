3df24c7c4d06a2217ea9a10a09674a1795d994bc
Siwa12100 <jean.marcillac12@gmail.com>
2023-11-29 19:14:41 +0100

    avancées sur le plugin...

--- Lobor/src/main/java/dev/siwa/lobor/Lobor.java
+++ Lobor/src/main/java/dev/siwa/lobor/Lobor.java
@@ -1,14 +1,19 @@
 package dev.siwa.lobor;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import dev.siwa.lobor.commands.giveHorse;
 
 public final class Lobor extends JavaPlugin {
 
+    private static Lobor instance;
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
         System.out.println("(Temporaire Lobor) : Activation du plugin lobor. \n");
-
+        Bukkit.getServer().getPluginManager().registerEvents(new MainListener(), this);
+        Bukkit.getServer().getPluginCommand("givehorse").setExecutor(new giveHorse());
     }
 
     @Override
