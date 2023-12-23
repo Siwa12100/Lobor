7a39d3740a5ca2801d0a0f409d4cb703ed7829f2
Siwa12100 <jean.marcillac12@gmail.com>
2023-12-23 12:22:36 +0100

    debut du projet

---
+++ Lobor/src/main/java/dev/siwa/lobor/Lobor.java
@@ -0,0 +1,17 @@
+package dev.siwa.lobor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lobor extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ Lobor/src/modele/montures/Monture.java
@@ -0,0 +1,7 @@
+package dev.siwa.lobor.modele.montures;
+
+public abstract class Monture {
+
+    // Joueur
+    private Player joueur;
+}
